06397450aa224d9f2d8ae5b77ffea238a66727f0
Kurt Grutzmacher <grutz@jingojango.net>
2014-05-26 10:15:18 -0700

    travis runnnnz

--- private/travis.sh
+++ private/travis.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -ev
 
-if [ "${TRAVIS_PULL_REQUEST}" -ne "false" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
   TRAVIS_DIR=$(pwd)
   cp kvasir.yaml.travis kvasir.yaml
-  cd ..
+  cd $HOME
   git clone https://github.com/web2py/web2py web2py
-  ln -s $TRAVIS_DIR web2py/applications/kvasir
+  ln -s $TRAVIS_BUILD_DIR web2py/applications/kvasir
   cd web2py
+  W2P_HOME=$(pwd)
+  PYTHONPATH=$W2P_HOME
   ./web2py -S kvasir -M -R applications/kvasir/private/user.py -A -u test -p test
+  cd applications/kvasir/modules/skaldship
+  nosetests --with-doctest
 fi
